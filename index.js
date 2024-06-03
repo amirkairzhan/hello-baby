1f201331009a02d753cc7cb1bdf04bdae7298b7c
amirkairzhan <amirkairzhan@bk.ru>
2024-06-03 21:44:46 +0900

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const outut = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    outut.innerHTML = 'У тебя самая красивая попка';
+});
